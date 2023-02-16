3f88fbe6ec5b4ffceddda285249d2b25b1622758
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-02-16 18:52:34 +0300

    last commit before compiling

--- config.def.h
+++ config.def.h
@@ -42,8 +42,10 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         0,          0,           0,        -1 },
 	{ "firefox", NULL,     NULL,           1 << 1,    0,          0,          -1,        -1 },
-	{ "thunderbird", NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
+	{ "thunderbird", NULL,     NULL,       1 << 5,    0,          0,          -1,        -1 },
+	{ "obsidian", NULL,     NULL,       1 << 4,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Gnome-terminal",  NULL,   NULL,     0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
